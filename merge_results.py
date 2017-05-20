2404b8b0aaca4e2fb4f1e1c2f58d53a2f9d9b22d
Nikolay <ndelko@mail.ru>
2017-05-20 22:11:17 +0300

    мерж написан, впереди проверка значений

--- merge_results.py
+++ merge_results.py
@@ -2,20 +2,36 @@
 
 from openpyxl import load_workbook
 from openpyxl import Workbook
+from openpyxl.cell import Cell
+import copy
 
-
-GAME_5STEP_DIR_NAMES={'08','15','22','01'}
-GAME_10STEP_DIR_NAMES={'11','18','25','29'}
+GAME_5STEP_DIR_NAMES=set()
+#GAME_5STEP_DIR_NAMES={'08',u'15',u'22',u'01'}
+#GAME_10STEP_DIR_NAMES={'11',u'18',u'25',u'29'}
+GAME_10STEP_DIR_NAMES={'11',u'18'}
 DIR_NAMES=GAME_5STEP_DIR_NAMES | GAME_10STEP_DIR_NAMES
 SHEET_NAMS=[u'Energy',u'Entropy',u'Hurst']
 BASE_DIR_PATH='D:\!informatics\projects\python\Stab_recount'
 SRC_FNAME_BEF='Before_Soc_EEH2.xlsx'
 SRC_FNAME_AFT='After_Soc_EEH2.xlsx'
 COL_CT_MAX=20
-GAME_10STEP_REF_COLNAMS=['1','1','2','2','3','3','4','4','5','5','6','6','7','7','8','8','9','9','10','10',
-                         '1','1','2','2','3','3','4','4','5','5','6','6','7','7','8','8','9','9','10','10']
-GAME_5STEP_REF_COLNAMS=['1','1','2','2','3','3','4','4','5','5','1','1','2','2','3','3','4','4','5','5',
-                        '1','1','2','2','3','3','4','4','5','5','1','1','2','2','3','3','4','4','5','5']
+COL_HEIGHT_MAX=11
+EMPTY_CELL_VAL=""
+IS_READONLY=False
+#IS_READONLY=True
+GAME_10STEP_REF_COLNAMS=[u'0', u'ГЗ', u'ГЗ_end', u'1',u'1',u'2',u'2',u'3',u'3',u'4',u'4',u'5',u'5',u'6',u'6',u'7',u'7',u'8',u'8',u'9',u'9',u'10',u'10',
+                         u'1',u'1',u'2',u'2',u'3',u'3',u'4',u'4',u'5',u'5',u'6',u'6',u'7',u'7',u'8',u'8',u'9',u'9',u'10',u'10', u'11',	u'Extr_end', u'ГО', u'ГЗ', u'ГЗ_end'
+]
+GAME_5STEP_REF_COLNAMS=[u'0', u'ГЗ', u'ГЗ_end', u'1',u'1',u'2',u'2',u'3',u'3',u'4',u'4',u'5',u'5',u'1',u'1',u'2',u'2',u'3',u'3',u'4',u'4',u'5',u'5',
+                        u'1',u'1',u'2',u'2',u'3',u'3',u'4',u'4',u'5',u'5',u'1',u'1',u'2',u'2',u'3',u'3',u'4',u'4',u'5',u'5', u'11',	u'Extr_end', u'ГО', u'ГЗ', u'ГЗ_end']
+
+class Empty_col:
+  def __init__(self):
+    pass
+
+class Empty_cell:
+  def __init__(self):
+    pass
 
 def fpath_bef(dir_nam):
   return BASE_DIR_PATH+'\\'+dir_nam+'\\'+SRC_FNAME_BEF
@@ -24,8 +40,8 @@ def fpath_aft(dir_nam):
 
 wb_bef_aft_list=[]
 for dir in DIR_NAMES:
-  wb_bef_aft_list.append((load_workbook(fpath_bef(dir), read_only=True),
-                          load_workbook(fpath_aft(dir), read_only=True),
+  wb_bef_aft_list.append((load_workbook(fpath_bef(dir), read_only=IS_READONLY),
+                          load_workbook(fpath_aft(dir), read_only=IS_READONLY),
                           dir in GAME_10STEP_DIR_NAMES))
 
 wb_merged = Workbook()
@@ -34,20 +50,61 @@ for nam in SHEET_NAMS:
 
 for nam in SHEET_NAMS:
   sheet_merged = wb_merged.get_sheet_by_name(nam)
+
   for wb_bef,wb_aft,game_10step in wb_bef_aft_list:
     sheet_bef = wb_bef.get_sheet_by_name(nam)
     sheet_aft = wb_aft.get_sheet_by_name(nam)
-    humans={row[0] for row in sheet_bef.iter_rows(min_row=2)} | \
-           {row[0] for row in sheet_aft.iter_rows(min_row=2)}
+    humans={row[1].value for row in sheet_bef.iter_rows(min_row=2)} | \
+           {row[1].value for row in sheet_aft.iter_rows(min_row=2)}
     humans=sorted(humans)
-    col_n=0
-    cols=[sheet_bef.columns()[0]]
-    for ref_nam in GAME_10STEP_REF_COLNAMS if game_10step else GAME_5STEP_REF_COLNAMS:
-      if ref_nam==col_nams[col_n]:
-        col_n+=1
-        cols.append(sheet_bef.columns()[col_n+1])
-      else:
-        cols.append(empty_col)
+    merged_cols_all={}
+    for coll_group_id,sheet in {('bef',sheet_bef),('aft',sheet_aft)}:
+      src_human_id={}
+      id=0
+      humans_cur={ cell.value for cell in list(sheet.columns)[1][1:]}
+      for human in humans:
+        if human in humans_cur:
+          src_human_id[human]=id
+          id+=1
+      col_n = 0
+      # первый столбец - этоимена людей, но первое значение - не имя, а название столюца с именами
+      cols = [[sheet['B1'].value]+humans]
+      col_nams = [unicode(i.value) for i in list(sheet.rows)[0][2:]]
+      for ref_nam in GAME_10STEP_REF_COLNAMS if game_10step else GAME_5STEP_REF_COLNAMS:
+        if ref_nam == col_nams[col_n]:
+          new_col=[ref_nam]
+          col=list(sheet.columns)[col_n + 2][1:]
+          col_n += 1
+
+          print "col_n="+str(col_n)
+          for human in humans:
+            print u"human="+human
+            if human in src_human_id:
+              print "index="+str(src_human_id[human])
+              new_col.append(col[src_human_id[human]].value)
+            else:
+              new_col.append(EMPTY_CELL_VAL)
+          cols.append(new_col)
+        else:
+          cols.append(Empty_col())
+      merged_cols_all[coll_group_id]=cols
+
+    for row_n in range(len(merged_cols_all['bef'][0])):
+      row_merged=[]
+      for cols in [merged_cols_all['bef'],merged_cols_all['aft']]:
+        for col_n in range(len(GAME_10STEP_REF_COLNAMS)+1):
+
+          if isinstance(cols[col_n],Empty_col):
+            row_merged.append(EMPTY_CELL_VAL)
+          else:
+            row_merged.append(cols[col_n][row_n])
+
+      sheet_merged.append(row_merged)
+
+wb_merged.save('fucking.xlsx')
+
+
+
 
 
 
